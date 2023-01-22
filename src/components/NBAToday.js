217400191a1663132a14496c86f91bf1a1c25b30
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-01-22 00:10:36 -0800

    Added title component with styling

---
+++ src/components/NBAToday.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NBAToday() {
+  return <div>NBAToday</div>;
+}
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Title() {
+  return (
+    <div className="title-main-div">
+      <h1 className="title-h1">FanDupe Sports</h1>
+      <h2 className="subtitle-h2">nothing to lose, nothing to gain</h2>
+    </div>
+  );
+}
