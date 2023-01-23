c348974d849d9ead5bebd5488804c1a74ccb5e8a
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2023-01-22 21:40:57 -0800

    Updated styling of title and today's games section to be responsive

--- src/components/NBAToday.js
+++ src/components/NBAToday.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+function generateTodaysGames() {
+  fetch(
+    "http://api.sportradar.us/nba/trial/v7/en/games/2023/01/23/schedule.json?api_key=pgh7vkkm4s2u2fdyfgzc6x9f"
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+}
+
 export default function NBAToday() {
-  return <div>NBAToday</div>;
+  //generateTodaysGames();
+  return (
+    <div className="today-body-div">
+      <div className="today-main-div">
+        <h1 className="today-h1">Today's Games</h1>
+      </div>
+    </div>
+  );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,9 +2,15 @@ import React from "react";
 
 export default function Title() {
   return (
-    <div className="title-main-div">
-      <h1 className="title-h1">FanDupe Sports</h1>
-      <h2 className="subtitle-h2">nothing to lose, nothing to gain</h2>
+    <div className="title-body-div">
+      <div className="title-main-div">
+        <h1 className="title-h1">
+          <span>FanDupe</span>
+          <br />
+          <span>Sports</span>{" "}
+        </h1>
+        <h3 className="subtitle-h3">nothing to lose, nothing to gain</h3>
+      </div>
     </div>
   );
 }
